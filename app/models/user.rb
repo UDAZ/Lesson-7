0f0ddbde3c06320ddb5995005d0c008f3e5c0d2b
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-05-04 13:44:06 +0000

    add formview

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ApplicationRecord
   def prefecture_name
     JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
   end
+  
   def prefecture_name=(prefecture_name)
     self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
   end
